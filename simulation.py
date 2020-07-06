1ffac828ab25ba0aaa68a74e159c75b9f896c881
Gabo Gálvez <gabo141298@gmail.com>
2020-07-05 23:18:13 -0600

    Eventos L2, E1 y E2

--- simulation.py
+++ simulation.py
@@ -18,6 +18,7 @@ class Simulation:
 		self.encargado2bDisponible = True
 
 		self.colaEsperaDesinfeccion = 0
+		self.colaEsperaEmpaquetado = 0
 
 		self.section1Queue = []
 		self.section2Queue = []
@@ -49,10 +50,10 @@ class Simulation:
 		if salida_mascara < 0.9:
 			t_llegada_s2 = self.clock + 1
 			self.section2Queue.append(t_llegada_s2)
-			if L2 == self.maxTime:
-				L2 = self.section2Queue[0]
+			if self.L2 == self.maxTime:
+				self.L2 = self.section2Queue[0]
 		else:
-			# Botar mascarilla
+			# Destruye mascarilla
 			# Aquí es donde se aumentan los contadores para estadísticas
 			1 == 1
 		if self.colaEsperaDesinfeccion > 0:
@@ -78,9 +79,83 @@ class Simulation:
 		else:
 			self.L1S2 = self.maxTime
 
+	def event_l2(self):
+		self.clock = self.L2
+		self.section2Queue.remove(self.L2)
+		self.colaEsperaEmpaquetado += 1
+		if self.colaEsperaEmpaquetado > 1 and (self.encargado2aDisponible or self.encargado2bDisponible):
+			self.colaEsperaEmpaquetado -= 2
+			encargado_elegido = 0
+			if self.encargado2aDisponible and self.encargado2bDisponible:
+				probabilidad_de_encargado = self.distribucion_uniforme.generate_random_value()
+				if probabilidad_de_encargado < 0.5:
+					encargado_elegido = 1
+				else:
+					encargado_elegido = 2
+			elif self.encargado1Disponible:
+				encargado_elegido = 1
+			else:
+				encargado_elegido = 2
+			
+			if encargado_elegido == 1:
+				# Genera valor para tiempo de servicio de encargado 1
+				self.E1 = self.clock + self.D3.generate_random_value()
+			elif encargado_elegido == 2:
+				# Genera valor para tiempo de servicio de encargado 2
+				self.E2 = self.clock + self.D4.generate_random_value()
+		if len(self.section2Queue) > 0:
+			self.L2 = self.section2Queue[0]
+		else:
+			self.L2 = self.maxTime
+
+	def event_e1(self):
+		self.clock = self.E1
+		destino_mascarilla = self.distribucion_uniforme.generate_random_value()
+		if destino_mascarilla < 0.05:
+			# Se botan las dos mascarillas
+			1 == 1
+		elif 0.05 <= destino_mascarilla and destino_mascarilla < 0.25:
+			# Se mandan a desinfectar de nuevo
+			llegada = self.clock + 2
+			self.section1Queue.append(llegada)
+			if self.L1S2 == self.maxTime:
+				self.L1S2 = self.section1Queue[0]
+		else:
+			# Se logran empaquetar y salen del sistema
+			1 == 1
+		if self.colaEsperaEmpaquetado > 1:
+			self.colaEsperaDesinfeccion -= 2
+			# Genera valor para tiempo de servicio de encargado 1
+			self.E1 = self.clock + self.D3.generate_random_value()
+		else:
+			self.E1 = self.maxTime
+
+	def event_e2(self):
+		self.clock = self.E2
+		destino_mascarilla = self.distribucion_uniforme.generate_random_value()
+		if destino_mascarilla < 0.15:
+			# Se botan las dos mascarillas
+			1 == 1
+		elif 0.15 <= destino_mascarilla and destino_mascarilla < 0.4:
+			# Se mandan a desinfectar de nuevo
+			llegada = self.clock + 2
+			self.section1Queue.append(llegada)
+			if self.L1S2 == self.maxTime:
+				self.L1S2 = self.section1Queue[0]
+		else:
+			# Se logran empaquetar y salen del sistema
+			1 == 1
+		if self.colaEsperaEmpaquetado > 1:
+			self.colaEsperaDesinfeccion -= 2
+			# Genera valor para tiempo de servicio de encargado 1
+			self.E2 = self.clock + self.D4.generate_random_value()
+		else:
+			self.E2 = self.maxTime
+
+
 	def start(self):
 		print('Inicia la simulación')
-		self.event_l1()
+		# Aquí hay que hacer el ciclo principal y eso
 
 	def read_distributions(self):
 		print('Considere las siguientes distribuciones:')
