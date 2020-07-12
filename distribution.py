e00fda15d7f7b15753cbf6f4e4a4ac64af0d9623
Gabo Gálvez <gabo141298@gmail.com>
2020-07-12 15:33:34 -0600

    Documentación de las clases de distribuciones y de encargados

--- distribution.py
+++ distribution.py
@@ -2,6 +2,9 @@ import math
 import random
 import numpy as np
 
+"""
+    Clase padre para los cinco tipos de distribuciones que se manejan.
+"""
 class Distribution:
     def generate_random_value(self):
         return 0
@@ -9,11 +12,16 @@ class Distribution:
     def read_distribution(self):
         print("")
 
+"""
+    Clase para la distribución uniforme.
+    En esta distribución se generan valores entre un a y un b, todos con el mismo chance de generarse.
+"""
 class Uniform(Distribution):
     def __init__(self, a = 0.0, b = 0.0):
         self.a = a
         self.b = b
-        print("Uniform: " + str(self.a) + " " + str(self.b))
+
+    # Genera un valor aleatorio entre a y b.
     def generate_random_value(self):
         # Se genera un número aleatorio entre 0 y 1
         r = random.random()
@@ -21,6 +29,7 @@ class Uniform(Distribution):
         # Se calcula x = r * (b-a+1) + a
         return (r * (self.b -self.a)) + self.a
 
+    # Método para leer el a y b de la entrada estándar, en caso de que no se haya definido al crear el objeto Uniform.
     def read_distribution(self):
         while True:
             self.a = float(input('\tPor favor digite el valor para a: '))
@@ -31,16 +40,20 @@ class Uniform(Distribution):
             else:
                 print("\tError, a debe ser un número menor que b\n")
 
+    # Método para cambiar los parámetros de a y b, en caso de que se ocupe cambiar o no se haya definido al principio.
     def set_parameters(self, a, b):
         self.a = a 
         self.b = b
 
-
+"""
+    Clase para la generación de valores aleatorios con distribución normal por método directo.
+"""
 class DirectNormal(Distribution):
     def __init__(self, mean = 0.0, variance = 0.0):
         self.mean = mean
         self.variance = variance
-        print("DirectNormal: " + str(self.mean) + " " + str(self.variance))
+
+    # Método que genera un valor aleatorio, a partir de otro valor aleatorio z. Este valor z sale usando coordenadas polares para encontrar su valor.
     def generate_random_value(self):
         # Se generan dos números aleatorios entre 0 y 1
         r_1 = random.random()
@@ -52,15 +65,21 @@ class DirectNormal(Distribution):
         # Aquí es con la fórmula de  x = sigma * z + mu
         return self.variance * z + self.mean
 
+    # Método para leer el mean y variance de la entrada estándar, en caso de que no se haya definido al crear el objeto DirectNormal.
     def read_distribution(self):
         self.mean = float(input('\tPor favor digite el valor para la media: '))
         self.variance = float(input('\tPor favor digite el valor para la varianza: '))
 
+"""
+    Clase para la generación de valores aleatorios con distribución normal por método de la convolución.
+"""
 class ConvolutionNormal(Distribution):
     def __init__(self, mean = 0.0, variance = 0.0):
         self.mean = mean
         self.variance = variance
-        print("ConvolutionNormal: " + str(self.mean) + " " + str(self.variance))
+    
+    # Método que genera un valor aleatorio, a partir de otro valor aleatorio z. 
+    # Este valor z sale sacando otros 12 valores aleatorios entre 0 y 1, sumándolos entre sí y a ese resultado restarle 6.
     def generate_random_value(self):
         z = 0
 
@@ -75,15 +94,20 @@ class ConvolutionNormal(Distribution):
         # Aquí es con la fórmula de  x = sigma * z + mu
         return self.variance * z + self.mean
     
+    # Método para leer el mean y variance de la entrada estándar, en caso de que no se haya definido al crear el objeto ConvolutionNormal.
     def read_distribution(self):
         self.mean = float(input('\tPor favor digite el valor para la media: ') )
         self.variance = float(input('\tPor favor digite el valor para la varianza: ') )
 
+"""
+    Clase para la generación de valores aleatorios con distribución exponencial.
+    Se obtienen valores por el método de transformación inversa con la distribución acumulativa de la exponencial.
+"""
 class Exponential(Distribution):
     def __init__(self, lambd = 0.0):
         self.lambd = lambd
-        print("Exponential: " + str(self.lambd))
 
+    # Método que genera un valor aleatorio usando transformación inversa sobre la función de distribución acumulativa de la exponencial.
     def generate_random_value(self):
         # Generamos un valor aleatorio entre 0 y 1
         r = random.random()
@@ -92,19 +116,24 @@ class Exponential(Distribution):
         # distribución exponencial con el ITM: x = -ln(r)/lambda
         return -1 * (np.log(r)/self.lambd)
 
+    # Método para leer el lambda si no se pudo definir al crear el objeto Exponential.
     def read_distribution(self):
         self.lambd = float(input('\tPor favor digite el valor para lambda: '))
 
+"""
+    Clase para la generación de valores aleatorios con distribución dada por la función f(x)=kx.
+    Se obtienen valores por el método de transformación inversa con la distribución acumulativa de la función f(x)=kx, es decir, F(x)=kx^2/2.
+"""
 class DensityFunction(Distribution):
     def __init__(self, a = 0.0, b = 0.0, k = 0.0):
         self.a = a
         self.b = b
         self.k = k
-        print("DensityFunction: " + str(self.a) + " " + str(self.b) + " " + str(self.k))
 
         # Para que r solo esté en el rango [F(a), F(b)], es decir, [k*a^2/2, k*b^2/2]
         self.uniform = Uniform(self.k * self.a * self.a / 2, self.k * self.b * self.b / 2)
 
+    # Genera un valor aleatorio usando el método de transformación inversa con F(x)=kx^2/2.
     def generate_random_value(self):
         # Se obtiene un r entre [F(a), F(b)]
         r = self.uniform.generate_random_value()
@@ -113,6 +142,7 @@ class DensityFunction(Distribution):
         # Al final se obtuvo que x = sqrt(2r / k)
         return math.sqrt( 2 * r / self.k )
 
+    # Método para leer el a, b y k de la entrada estándar, en caso de que no se haya definido al crear el objeto DensityFunction.
     def read_distribution(self):
         while True:
             self.a = float(input('\tPor favor digite el valor para a: '))
--- employee.py
+++ employee.py
@@ -1,40 +1,60 @@
 from mask import Mask
 
+"""
+    Clase padre para los dos tipos de encargados que existen en el sistema, el de la sección 1 y los de la sección 2.
+    Aquí se tiene la parte que tienen en común los encargados, que tienen que ir acumulando su tiempo de servicio, 
+    para obtener el tiempo que pasan trabajando
+"""
 class Employee:
+
     def __init__(self, warm_up_time):
         self.disponible = True
         self.init_time = 0
         self.acum_service_time = 0
-
         self.warm_up_time = warm_up_time
+
+    # Método donde se marca cuando inició a atender a las mascarillas.
     def start_service(self, init_time):
         self.init_time = init_time
         self.disponible = False
+
+    # Método donde se termina de atender a las mascarillas y se aumenta en el acumulado de tiempos de servicio.
     def end_service(self, end_time):
         if self.init_time > self.warm_up_time:
             self.acum_service_time += (end_time - self.init_time)
         self.init_time = 0
         self.disponible = True
 
+"""
+    Clase para el empleado de la sección 1, que solo tiene de cliente a una única máscara.
+"""
 class EmployeeSection1(Employee):
     def __init__(self, warm_up_time):
         super().__init__(warm_up_time)
         self.mask = None
+
+    # Método que se llama al empezar a atender a una máscara. Aquí se inicia el tiempo de servicio para la máscara y el encargado.
     def set_current_mask(self, init_time, mask):
         self.mask = mask
         self.mask.start_service_time(init_time)
         super().start_service(init_time)
 
+    # Método que se llama cuando el servicio termina. Se llama a terminar el tiempo de servicio de la máscara y del encargado, para aumentar los acumuladores.
     def service_ends(self, end_time):
         self.mask.end_service_time(end_time)
         self.mask = None
         super().end_service(end_time)
 
+"""
+    Clase para los empleados de la sección 2, que tienen de cliente parejas de máscaras.
+"""
 class EmployeeSection2(Employee):
     def __init__(self, warm_up_time):
         super().__init__(warm_up_time)
         self.mask1 = None
         self.mask2 = None
+
+    # Método que se llama al empezar a atender dos máscaras. Aquí se inicia el tiempo de servicio para las máscaras y el encargado.
     def set_current_masks(self, init_time, mask1, mask2):
         self.mask1 = mask1
         self.mask2 = mask2
@@ -43,6 +63,7 @@ class EmployeeSection2(Employee):
         self.mask1.start_service_time(init_time)
         self.mask2.start_service_time(init_time)
 
+    # Método que se llama cuando el servicio termina. Se llama a terminar el tiempo de servicio de las máscaras y del encargado, para aumentar los acumuladores.
     def service_ends(self, end_time):
         self.mask1.end_service_time(end_time)
         self.mask2.end_service_time(end_time)
